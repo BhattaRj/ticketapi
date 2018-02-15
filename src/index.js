4676e0646d4d75b869981fafbd0fed3dad777127
janak <janakrajbhatta@gmail.com>
2018-02-15 08:30:00 +0545

    port changed

--- src/index.js
+++ src/index.js
@@ -49,4 +49,4 @@ app.use((err, req, res, next) => {
 })
 
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
